96f04e1029e73e4bcdbb45ec96df1d02834ca1fa
RuspeciaNervld <windnegev@qq.com>
2023-07-02 12:13:12 +0800

    other

---
+++ src/main/java/com/calculator/Main.java
@@ -0,0 +1,9 @@
+package com.calculator;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(CalculatorApplication.class, args);
+    }
+}
